eb2f199d064c1503b31d81b445df4db05f96ff94
Pavel Vershinin <xr.pavel@yandex.ru>
2017-08-04 06:47:52 +0300

    Новый метод, добавления шаблона

--- templates.go
+++ templates.go
@@ -1,6 +1,8 @@
 package globals
 
 type Templateser interface {
+	AddTemplate(tplName, tpl string)
 	Load(tplName string, data interface{}) (result string)
 	ParseTemplate(tpl, tplName string, data interface{}) (result string)
 }
+
