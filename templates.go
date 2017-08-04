5313ff6041576e247044bacdc7650a31b12a4ca0
Pavel Vershinin <xr.pavel@yandex.ru>
2017-08-04 07:05:07 +0300

    Убрал

--- templates.go
+++ templates.go
@@ -1,7 +1,6 @@
 package globals
 
 type Templateser interface {
-	AddTemplate(tplName, tpl string)
 	Load(tplName string, data interface{}) (result string)
 	ParseTemplate(tpl, tplName string, data interface{}) (result string)
 }
