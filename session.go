dbfdf3178e4648e21ca6bec1790b43c805d32197
Pavel Vershinin <xr.pavel@yandex.ru>
2017-09-23 19:25:06 +0300

    Logout and AuthAs

--- session.go
+++ session.go
@@ -3,6 +3,8 @@ package globals
 type Sessioner interface {
 	SocAuth() bool
 	Auth(username, password string, remember bool) bool
+	AuthAs(userID int64, remember bool) bool
+	Logout()
 	User() User
 	IsAuth() bool
 	IsMobile() bool
